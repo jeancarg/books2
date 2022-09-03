e382c33c13d630c26266c19afc919954aafac25c
jeancarg <109957146+jeancarg@users.noreply.github.com>
2022-09-03 14:49:05 -0300

    atualzação meu código

--- index.php
+++ index.php
@@ -5,118 +5,126 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Books</title>
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="css/layout.css">
-    <script src="js/updateMark.js" defer></script>
-    <script src="js/book.js" defer></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+
+    <script src="js/logica.js"></script>
+
+      <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
-    <header class="p-relative">
-        <aside class="p-absolute">
-            <form action="">
-                <div class="form-group">
-                    <label for="txt_email">e-mail</label>
-                    <input type="email" name="txt_email"
-                        id="txt_email" class="form-control">
+
+   <header>
+       
+       <aside>
+           <form action="">
+               <div class="form-group">
+                   <label for="txtEmail"></label>
+                   <input type="email" name="txtEmail" id="txtEmail" class="form-control" placeholder="Ex.: @jeancar">
                 </div>
-                <div class="form-group">
-                    <label for="txt_senha">Senha</label>
-                    <input type="password" name="txt_senha"
-                        id="txt_senha" class="form-control">
+
+                   <div class="form-group">
+                    <label for="senha"></label>
+                    <input type="password" name="senha" id="senha" class="form-control" placeholder="Ex.:fgjgfdhb">
                 </div>
-                <a href="#">Esqueci a senha</a>
-                <div class="form-group">
-                    <input type="button" value="Login"
-                        class="btn btn-primary">
-                    <input type="button" value="Cadastre-se"
-                        class="btn btn-primary">
+
+                <a href="">Esqueci minha senha </a>
+
+                <div>
+                    <input type="submit" value="Login" class="btn btn-primary">
+                    <a href="" class="btn btn-primary">Cadastrar-se</a>
                 </div>
-            </form>
-        </aside>
-        <h1>
-            <?= "Books" ?>
-        </h1>
-        <h2>
-            <?php 
-            echo "Os mais temidos";
-            ?>
-        </h2>
-    </header>
-    <main>
-        <form action="" class="form-inline">
-            <div class="form-group">
-                <input type="text" name="txt_livro"
-                    class="form-control" id="txt_livro">
-                <input type="button" value="Salvar"
-                    class="btn btn-primary" id="btn_salvar">
+
+           </form>
+       </aside>
+       <h1>
+           <?= 
+           "Books"
+           ?>
+       </h1>
+
+       <h2>
+           <?php 
+           echo "Os Livros de Renato";
+           ?>
+       </h2>
+   </header>
+
+   <main>
+       <form action="" class="form-inline">
+           <div class="form-group">
+               <input type="text" name="txtLivro" id="txtLivro" class="form-control" placeholder="pesquise um livro">
+               <input type="button" value="Salvar" class="btn btn-primary" onclick="criarHTML('txtLivro')">
+
             </div>
-        </form>
-        <div id="livros">
-            <?php
-            require_once "model/Conexao.php";
+       </form>
 
-            $sql = "select * from book;";
+       <section id="sectionLivros">
+           <?php
+           require_once "model/Conexao.php";
 
-            if(!Conexao::execWithReturn($sql)){
-                echo Conexao::getErro();
-                exit(1);
-            }
-            //print_r(Conexao::getData());
-            $dados = Conexao::getData();
-            //print_r($dados);
-            //foreach($dados as $livro){
-            foreach($dados as $livro):
-            
+           $sql = "select * from book;";
+
+           if(!Conexao::execWithReturn($sql)){
+               echo Conexao::getData();
+               exit(1);
+           }
+           $dados = Conexao::getdata();
+
+           foreach($dados as $livro):
+
             ?>
-            <section class="d-flex">
-                <div class="livro-imagem">
+           <article>
+               <div>
                     <img src="img/livro.webp" alt="Imagem do livro">
-                </div>
-                <div class="livro-contexto">
-                    <p class="livro-dados">
-                        Livro:
-                        <span id="livro-nome">
-                            <?= $livro["nome"]; ?>
-                        </span>
-                    </p>
-                    <p class="livro-dados">
-                        Páginas:
-                        <span id="livro-paginas">
-                            <?= $livro["paginas"]; ?>
-                        </span>
-                    </p>
-                    <p class="livro-dados">
-                        Autor/a/as/es:
-                        <span id="livro-autores">
-                            <?= $livro["autor"]; ?>
-                        </span>
-                    </p>
-                </div>
-                <div class="livro-marcos">
-                    <div onclick="updateMark(this)">
-                        <span class="material-icons">
-                            book
-                        </span>
-                        <span class="round">
-                            12
-                        </span>
+               </div>
+
+               <div class="detalhes">
+                   <h3>Livro: <span>
+                    <?= $livro=["nome"]; ?>
+                   </span></h3>
+                   <h3>Páginas: <span>
+                    <?= $livro=["paginas"]; ?>
+                   </span></h3>
+                   <h3>Autor/a/as/es: <span>
+                    <?= $livro=["autor"]; ?>
+                   </span></h3>
+               </div>
+
+               <div>
+                   <div class="marcadores">
+                       <span class="material-icons">
+                           book
+
+                       </span>
+                       <span class="contador rounded-circle">
+                           12
+
+                       </span>
                     </div>
-                    <div onclick="updateMark(this)">
+
+                    <div class="marcadores">
                         <span class="material-icons">
                             favorite
+ 
                         </span>
-                        <span class="round">
-                            12
+                        <span class="contador rounded-circle" >
+                            15
+ 
                         </span>
-                    </div>
-                </div>
-            </section>
-            <?php
+                     </div>
+
+               </div>
+
+           </article>
+           <?php
             //}//foreach
             endforeach;
             ?>
-        </div>
-    </main>
+       </section>
+   </main>
+    
 </body>
 </html>
